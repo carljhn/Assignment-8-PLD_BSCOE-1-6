51f8a4f9b628fb01ef3fb06699884451a1e83bfe
carljhn <zoletacarljohn1@gmail.com>
2021-12-05 09:03:27 -1200

    Second commit: I have added the module for this program and the variables for the input and random number

--- guessNum.py
+++ guessNum.py
@@ -4,3 +4,6 @@
 #Display “Greater than” if the inputed number is greater than the random number
 #Display “Less than” if the inputed number is less than the random number
 #Repeat asking the user until the random number has been guessed correctly.
+import random
+ranNum=random.randint(0, 100)
+guessNum=int(input("Guess the number: "))
